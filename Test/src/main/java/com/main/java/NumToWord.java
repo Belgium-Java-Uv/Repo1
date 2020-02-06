376648085bcccb1e3d388901980a6fb9d7168796
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-06 18:12:12 +0530

    program can now accept all the required range of numbers with multiple scenarios

--- Test/src/main/java/com/main/java/NumToWord.java
+++ Test/src/main/java/com/main/java/NumToWord.java
@@ -1,8 +1,49 @@
 package com.main.java;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class NumToWord {
+
+	private static final String[] specialNames = { "", " thousand"
+
+			// Below values can be enabled as per the requirement of the range
+			// if it has to exceed 999999
+			// Also the Pattern needs to be modified if the range has to exceed
+			// the limit 999999
+
+			/*
+			 * , " million"," billion", " trillion", " quadrillion",
+			 * " quintillion"
+			 */
+
+	};
+
+	public static final String[] tensNames = { "", " ten", " twenty", " thirty", " forty", " fifty", " sixty",
+			" seventy", " eighty", " ninety" };
+
+	public static final String[] numNames = { "", " one", " two", " three", " four", " five", " six", " seven",
+			" eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen",
+			" seventeen", " eighteen", " nineteen" };
+
+	private String convertLessThanOneThousand(int intNumber) {
+		String current;
+
+		if (intNumber % 100 < 20) {
+			current = numNames[intNumber % 100];
+			intNumber /= 100;
+		} else {
+			current = numNames[intNumber % 10];
+			intNumber /= 10;
+
+			current = tensNames[intNumber % 10] + current;
+			intNumber /= 10;
+		}
+		if (intNumber == 0)
+			return current;
+		return numNames[intNumber] + " hundred" + current;
+	}
 
 	public String convert(String strNumber) {
 
@@ -11,14 +52,35 @@ public class NumToWord {
 		if (intNumber == 0) {
 			return "zero";
 		}
-		return "";
+
+		String prefix = "";
+
+		if (intNumber < 0) {
+			intNumber = -intNumber;
+			prefix = "negative";
+		}
+
+		String current = "";
+		int place = 0;
+
+		do {
+			int n = intNumber % 1000;
+			if (n != 0) {
+				String s = convertLessThanOneThousand(n);
+				current = s + specialNames[place] + current;
+			}
+			place++;
+			intNumber /= 1000;
+		} while (intNumber > 0);
+
+		return (prefix + current).trim();
 
 	}
 
 	public String checkInputAndConvert(String strNumber) {
 
 		if (strNumber != null && !strNumber.equals("")) {
-			String pattern = "[-+]?[0]*";
+			String pattern = "[-+]?[0]*|[-+]?[0]*[1-9][0-9]{0,5}";
 
 			boolean isValidInput = Pattern.matches(pattern, strNumber);
 			if (isValidInput) {
@@ -31,5 +93,4 @@ public class NumToWord {
 			return "Invalid input";
 		}
 	}
-
 }
--- Test/src/test/java/com/test/java/NumToWordTest.java
+++ Test/src/test/java/com/test/java/NumToWordTest.java
@@ -29,4 +29,37 @@ public class NumToWordTest {
 		assertEquals("zero", numToWord.checkInputAndConvert("-000"));
 		assertEquals("zero", numToWord.checkInputAndConvert("+000"));
 	}
+	
+	@Test
+	public final void whenSixDigitsIsSuppliedWithOrWithoutLeadingZeros() {
+		assertEquals("one hundred twenty three thousand four hundred fifty six",
+				numToWord.checkInputAndConvert("123456"));
+		assertEquals("one hundred twenty three thousand four hundred fifty six",
+				numToWord.checkInputAndConvert("00123456"));
+	}
+
+	@Test
+	public final void whenValueIsSuppliedWithLeadingMinus() {
+		assertEquals("negative nine hundred ninety nine thousand nine hundred ninety nine", numToWord.checkInputAndConvert("-999999"));
+	}
+	
+	@Test
+	public final void whenValueIsSuppliedWithTrailingPlus() {
+		assertEquals("Invalid input", numToWord.checkInputAndConvert("999999+"));
+	}
+
+	@Test
+	public final void whenOtherThanNumberIsSupplied() {
+		assertEquals("Invalid input", numToWord.checkInputAndConvert("9abc99$#"));
+	}
+
+	@Test
+	public final void whenMaxLimitValueIsSupplied() {
+		assertEquals("nine hundred ninety nine thousand nine hundred ninety nine", numToWord.checkInputAndConvert("999999"));
+	}
+	
+	@Test
+	public final void whenValueMoreThanMaxLimitIsSupplied() {
+		assertEquals("Invalid input", numToWord.checkInputAndConvert("1000000"));
+	}
 }
