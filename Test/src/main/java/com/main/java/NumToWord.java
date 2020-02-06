80657dd98224f9c01b28eb5f711e3b8d2b9a1dc4
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-06 17:57:47 +0530

    Committing source code to pass the 1st test case

---
+++ Test/src/main/java/com/main/java/NumToWord.java
@@ -0,0 +1,35 @@
+package com.main.java;
+
+import java.util.regex.Pattern;
+
+public class NumToWord {
+
+	public String convert(String strNumber) {
+
+		int intNumber = Integer.parseInt(strNumber.toString()); // ConvertsToIntValueAndAlsoRemovesLeadingZerosIfAny;
+
+		if (intNumber == 0) {
+			return "zero";
+		}
+		return "";
+
+	}
+
+	public String checkInputAndConvert(String strNumber) {
+
+		if (strNumber != null && !strNumber.equals("")) {
+			String pattern = "[0]*";
+
+			boolean isValidInput = Pattern.matches(pattern, strNumber);
+			if (isValidInput) {
+				return convert(strNumber);
+			} else {
+				return "Invalid input";
+			}
+
+		} else {
+			return "Invalid input";
+		}
+	}
+
+}
