811443a24cb499bb4467f128b9f5072c1c94fbe9
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-24 15:55:12 +0530

    Committing the constants file containing PLAYER_1 as a constant

---
+++ TennisGame/src/main/java/com/game/tennis/util/TennisGameConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.game.tennis.util;
+
+/**
+ * This interface contains the constants used in the Tennis Game application
+ * @author 2020-DEV-047
+ *
+ */
+public interface TennisGameConstants {
+	String PLAYER_1 = "Player_1";
+}
